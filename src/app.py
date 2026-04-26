af058947b593e59a7598cb08e1535279433342fc
Francesco Sacco <francescosacco.github@gmail.com>
2026-04-26 12:28:53 +0200

    docs(app): update comments for better clarity on the run function

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
+# Importing libraries to configure and run the MainWindow
 import sys
 from PyQt6.QtWidgets import QApplication
 from src.components.main_window import MainWindow
 
 
+# Start the application enabling exit with cross icon at the top
 def run():
-    app = QApplication(sys.argv)
+    app: QApplication = QApplication(sys.argv)
 
-    window = MainWindow()
+    # Instantiating the window and it's size
+    window: MainWindow = MainWindow()
     window.resize(800, 500)
     window.show()
 
