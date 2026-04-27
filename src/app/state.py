62fa61330e8e549780ef37e1dddec5a492b39625
Francesco Sacco <francescosacco.github@gmail.com>
2026-04-27 19:10:15 +0200

    feat(state): add SessionState class with default view and schedule

--- src/app/state.py
+++ src/app/state.py
@@ -13,3 +13,9 @@ class ViewState:
 def create_default_view_state(today: date | None = None) -> ViewState:
     today: date | None = today or date.today()
     return ViewState(selected_month=today.replace(day=1), selected_day=today)
+
+
+@dataclass(slots=True)
+class SessionState:
+    scheudle: Schedule = field(default_factory=Schedule)
+    view_state: ViewState = field(default_factory=create_default_view_state)
